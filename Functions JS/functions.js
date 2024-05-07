859e35228f85021a31f666d31c6ce26b93b4429a
MoosaHaroon6 <157456649+MoosaHaroon6@users.noreply.github.com>
2024-05-07 19:01:47 +0500

    Add files via upload
    functions and Objects solutions

---
+++ Functions JS/functions.js
@@ -0,0 +1,206 @@
+//   {html
+// CSS   (boot-strap)
+// Java-Script
+// Advanced JS
+// Type-Script
+// Git
+// FireBase
+// Next JS (React JS)
+// Node (Nest JS)
+// Express 
+//   }
+
+
+
+
+
+
+// ``  for multiline concatination 
+// $ {} for multiline concatination
+
+
+
+
+
+
+
+
+
+
+
+
+// function  findDivision (percentage) {
+// let division ;
+// if (percentage >= 60){
+// division = "First Division"
+// }
+// else if (percentage <= 60) {
+// division = "Second Division"
+// }
+// else {
+// division = "Failed"
+// }
+// return division ;
+// } 
+// 
+
+
+
+
+
+
+
+
+
+// function calculateHyp (base , perp){
+// 
+// function calculatesqr(num){
+// base * base , perp * perp
+//    }
+// calculatesqr = Math.sqrt(num)
+// 
+// }
+// function calculateHyp (23 ,32)
+// let num = calculateHyp (4,4);
+
+
+
+
+
+
+
+
+
+
+// function findlargestnsmallestval() {
+    // let lar = arguments[0];
+    // let small = arguments[0];
+    // for (let i = 0; i < arguments.length; i++) {
+        // if (arguments[i] > lar) {
+            // lar = arguments[i];
+        // }
+        // if (arguments[i] < small) {
+            // small = arguments[i];
+        // }
+// 
+    // }
+    // console.log(lar, small);
+    
+// }
+// 
+// findlargestnsmallestval(12, 232, 34,0.34,324,343,342,7657);
+
+
+
+
+
+
+
+
+// function sum (arrofnum){
+    // let arrNum= arrofnum.split("+");
+    // 
+    // console.log ();
+// }
+// sum ([12,212,3])
+
+
+
+
+
+
+
+
+// function joinword (str,sept){
+    // let sept = "_"
+    // for( let i= 0; i < str.length ;i++){
+        // console.log (str[i]+sept)
+    //    let new = str[i]+sept;
+    //    return new;
+    // }
+// }
+// 
+// joinword ("moosa","-");
+
+// console.log ("M_O_O_S_A")
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let phoneBrands = ["Samsung" , "Nokia" ,"Apple" , "Huawei", "QMobile"];
+// 
+// for (let i = 0 ; i < phoneBrands.length ; i++){
+    // console.log(phoneBrands[i]);
+// }
+
+
+
+
+
+
+
+
+
+// let fruits = [ "Mango","Banana","Strawberry","Apple", "orange"] ;
+// 
+// for (let i = 0 ; i < fruits.length ; i++){
+    //  console.log(fruits[i]);
+// }
+
+// console.log("Element At 0 index is " + fruits[0]);
+// console.log("Element At 0 index is " + fruits[1]);
+// console.log("Element At 0 index is " + fruits[2]);
+// console.log("Element At 0 index is " + fruits[3]);
+// console.log("Element At 0 index is " + fruits[4]);
+
+
+
+
+
+
+
+
+
+
+let userInput = +prompt (`Enter Your Number`);
+   (userInput !== String || userInput !== 0)
+let n = prompt (`Enter Your Item`);
+
+for ( let i = 0 ; i < n ; i++){
+    console.log("Your Array + ");
+}
+
+
+
+
+
+
+
+
+// let userInput = prompt("Enter the number of items in the array:");
+// let n = parseInt(userInput); // Convert string input to number
+// 
+Check if user entered a valid number
+// if (isNaN(n) || n <= 0) {
+//   console.error("Invalid input: Please enter a positive number.");
+// } else {
+//   let myArray = [];
+//   
+  Loop n times to get item values from prompts
+//   for (let i = 0; i < n; i++) {
+    // let item = prompt(`Enter item ${i + 1}:`);
+    // myArray.push(item);
+//   }
+//   
+//   console.log("Your array:", myArray);
+// }
+// 
---
+++ objects in JS/objects.js
@@ -0,0 +1,104 @@
+//  Objects //
+
+
+// let studentData = {
+// studentName: "Moosa",
+// fathersName: "Haroon",
+// rollNo: 29581,
+// class: "XI"
+// }
+// 
+// console.log(studentData);
+ 
+ 
+ 
+ 
+
+
+
+
+// when we want to add something in obj
+// then,
+
+
+// {{{}}}
+
+
+// 
+// studentData.fee = true; 
+// studentData.classActivity = "Good" ;
+// 
+// console.log(studentData);
+
+
+
+
+
+
+
+// let age = +prompt("your age")
+// 
+// 
+// const stuData = {
+    // name: prompt("ENTER YOUR NAME?"),
+    // rollNo: prompt("ENTER YOUR RollNo?"),
+    
+// 
+// }
+// 
+// console.log(stuData,age);
+
+
+
+
+
+
+
+
+
+// function createBook (name,pages,author,topic){
+    // let book = {
+        // bookName : name ,
+        // TopicName :topic,
+        // pages   ,
+        // author 
+// 
+    // }
+    // return book;
+    // console.log(createBook("Java-Script"));
+// }
+// 
+// let book1 =createBook("Java-Script","90","Josh","Objects")
+// console.log(book1);
+// 
+
+
+
+
+
+
+
+
+
+// function doit (a,b,cb){
+    // console.log(a + b);
+// }
+// doit (10,12);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
